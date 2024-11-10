93b88be9c89586ec1dd3e3f56df0bac978cb0688
Abduvohid <vohidqurbobov606@gmail.com>
2024-11-10 23:37:54 +0500

    birinchi fayl qo'shildi
    boshlang'ich masalalar yechimlari yuklandi

---
+++ 40_boshlang'ich_masalalar.py
@@ -0,0 +1,335 @@
+import math
+# 1-masala
+# a = int(input("Kvadratning tomonini kiriting: a="))
+# P=4*a
+# print("Tomoni", str(a), "ga teng bo'lgan kvadratning peremetri " + str(P) + " ga teng.")
+
+# 2-masala
+# a = int(input("Kvadratning tomonini kiriting: a="))
+# S = a**2
+# print("Tomoni", str(a), "ga teng bo'lgan kvadratning yuzasi " + str(S) + " ga teng.")
+
+# 3-masala
+# a = int(input("To'g'ri to'rtburchakning birinchi tomonini kiriting: a="))
+# b = int(input("To'g'ri to'rtburchakning ikkinchi tomonini kiriting: b="))
+# S = a*b
+# P = 2*(a+b)
+# print("Tomonlari a="+str(a)+" va b="+str(b)+" bo'lgan to'g'ri to'rtburchakning yuzasi "+str(S)+" ga teng.")
+# print("Tomonlari a="+str(a)+" va b="+str(b)+" bo'lgan to'g'ri to'rtburchakning peremetri "+str(P)+" ga teng.")
+
+# 4-masala
+# d = int(input("Aylananing diametrini kiriting d="))
+# L = math.pi*d
+# print('Diametri '+str(d)+' ga teng bo\'lgab aylananing uzunligi ' + str(L)+ ' ga teng.')
+
+# 5-masala
+# a = int(input("Kubning tomonini kiriting a="))
+# V=a**3
+# S=6*a**2
+# print("Tomoni "+ str(a) + " ga teng bo'lgan kubning hajmi "+ str(V)+ " ga teng.")
+# print("Tomoni "+ str(a) + " ga teng bo'lgan kubning yuzasi "+ str(S)+ " ga teng.")
+
+# 6-masala
+# a = int(input("Paralelepepedning 1-tomonini kiriting a="))
+# b = int(input("Paralelepepedning 2-tomonini kiriting b="))
+# c = int(input("Paralelepepedning 3-tomonini kiriting c="))
+# V=a*b*c
+# S=2*(a*b+b*c+a*c)
+# print("Paralelepepedning hajmi "+str(V) +" ga teng.")
+# print("Paralelepepedning yuzasi "+str(S) +" ga teng.")
+
+# 7-masala
+# R = int(input("Doiraning radiusini kiriting: R="))
+# L = 2*math.pi*R
+# S = math.pi*R**2
+# print("Doiraning uzunligi "+str(L)+" ga teng.")
+# print("Doiraning yuzasi "+str(S)+" ga teng.")
+
+# 8-masala
+# a = float(input("1-sonni kiriting: a="))
+# b = float(input("2-sonni kiriting: b="))
+# c = (a+b)/2
+# print("Bu ikki sonning o'rta arifmetigi "+str(c)+" ga teng.")
+
+# 9-masala
+# a = float(input("1-sonni kiriting: (musbat) a="))
+# b = float(input("2-sonni kiriting: (musbat) b="))
+# c = math.sqrt(a*b)
+# print("Bu ikki sonning o'rta geometrigi "+str(c)+" ga teng.")
+
+# 10-masala
+# a = float(input("1-sonni kiriting: (nolga teng emas) a="))
+# b = float(input("2-sonni kiriting: (nolga teng emas) b="))
+# c = a+b
+# d = a*b
+# e = a**2+b**2
+# print(c)
+# print(d)
+# print(e)
+
+# 11-masala
+# a = float(input("1-sonni kiriting: (nolga teng emas) a="))
+# b = float(input("2-sonni kiriting: (nolga teng emas) b="))
+# c = a+b
+# d = a*b
+# e = abs(a)
+# f = abs(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+# 12-masala
+# a = float(input("1-katetni kiriting: (nolga teng va manfiy emas) a="))
+# b = float(input("1-katetni kiriting: (nolga teng va manfiy emas) b="))
+# c = math.sqrt(a**2+b**2)
+# p = a+b+c
+# print(c)
+# print(p)
+
+# 13-masala
+# r1 = int(input())
+# r2 = int(input())
+# s1 = math.pi*r1**2
+# s2 = math.pi*r2**2
+# s3 = s1-s2
+# print(s1)
+# print(s2)
+# print(s3)
+
+# 14-masala
+# L = int(input())
+# R =  L/(2*math.pi)
+# d= 2*R
+# print(R)
+# print(d)
+
+# 15-masala
+# s = int(input())
+# R = math.sqrt(s/math.pi)
+# d=2*R
+# print(R)
+# print(d)
+
+# 16-masala
+# x1 = float(input())
+# x2 = float(input())
+# x = abs(x2-x1)
+# print(x)
+
+# 17-masala
+# x1 = float(input())
+# x2 = float(input())
+# x3 = float(input())
+# ac = x3-x1
+# bc = x3-x2
+# print(ac)
+# print(bc)
+
+# 18-masala
+# x1 = float(input())
+# x3 = float(input())
+# x2 = float(input())
+# x= (x3-x1)*(x2-x3)
+# print(x)
+
+# 19-masala
+# x1 = int(input())
+# x2 = int(input())
+# y1 = int(input())
+# y2 = int(input())
+# S=(x2-x1)*(y2-y1)
+# P=2*(x2-x1+y2-y1)
+# print(S)
+# print(P)
+
+# 20-masala
+# x1 = int(input())
+# x2 = int(input())
+# y1 = int(input())
+# y2 = int(input())
+# x=math.sqrt((x2-x1)**2+(y2-y1)**2)
+# print(x)
+
+# 21-masala
+# x1 = int(input())
+# x2 = int(input())
+# y1 = int(input())
+# y2 = int(input())
+# z1 = int(input())
+# z2 = int(input())
+# a = math.sqrt((x2-x1)**2+(y2-y1)**2)
+# b = math.sqrt((z2-z1)**2+(y2-y1)**2)
+# c = math.sqrt((x2-x1)**2+(z2-z1)**2)
+# p = (a+b+c)/2
+# S = math.sqrt(p*(p-a)*(p-b)*(p-c))
+# print(S)
+# print(p)
+
+# 22-masala
+# 1-usul
+# a=4
+# b=5
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a)
+# print(b)
+# 2-usul
+# a=5
+# b=6
+# b=a
+# c=6
+# a=c
+# print(a)
+# print(b)
+
+# 23-masala
+# 1-usul
+# a=4
+# b=5
+# c=6
+# b=c
+# c=a
+# d=5
+# a=d
+# print(a)
+# print(b)
+# print(c)
+
+# 24-masala
+# a=9
+# b=35
+# c=90
+# b=c
+# c=a
+# d=35
+# a=d
+# print(a)
+# print(b)
+# print(c)
+
+# 25-masala
+# x = int(input())
+# y = 3*x**6-6*x**2-7
+# print(y)
+
+# 26-masala
+# x = int(input())
+# y = 4*(x-3)**6-7*(x-3)**3+2
+# print(y)
+
+# 27-masala
+# a= int(input())
+# b = a**2
+# c = a**4
+# d = a**8
+# print(b)
+# print(c)
+# print(d)
+
+# 28-masala
+# a = int(input())
+# b = a**2
+# c = a**3
+# d = a**5
+# e = a**10
+# f = a**15
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+# 29-masala
+# a = int(input("0 dan 360 gacha bo'lgan ixtiyoriy gradusni kiriting:"))
+# radian = a* math.pi/180
+# print(radian)
+
+# 30-masala
+# radian = float(input())
+# a = radian*180/math.pi
+# print(a)
+
+# 31-masala
+# Tf = float(input())
+# Tc = (Tf-32)*5/9
+# print(Tc)
+
+# 32-masala
+# Tc = float(input())
+# Tf = 9*Tc/5+32
+# print(Tf)
+
+# 33-masala
+# X = int(input())
+# A = int(input())
+# Y = int(input())
+# Z = 1/X*A
+# K = Y/X*A
+# print(Z)
+# print(K)
+
+# 34-masala
+# X = int(input())
+# A = int(input())
+# Y = int(input())
+# B = int(input())
+# bir_sh = 1/X*A
+# bir_k = 1/Y*B
+# farq = abs(bir_sh-bir_k)
+# print(farq)
+
+# 35-masala
+# V = int(input())
+# U = int(input())
+# T1 = int(input())
+# T2 = int(input())
+# Sob = (U+V)*T1
+# Soq = (U-V)*T2
+# print(Sob)
+# print(Soq)
+
+# 36-masala
+# V1 = int(input())
+# V2 = int(input())
+# T = int(input())
+# S = (V1+V2)*T
+# print(S)
+
+# 37-masala
+# V1 = int(input())
+# V2 = int(input())
+# T = int(input())
+# S = (V1-V2)*T
+# print(S)
+
+# 38-masala
+# A = int(input())
+# B = int(input())
+# x = -B/A
+# print(x)
+
+# 39-masala
+# A = int(input())
+# B = int(input())
+# C = int(input())
+# D=B**2-4*A*C
+# X1 = (-B + math.sqrt(D))/(2*A)
+# X2 = (-B - math.sqrt(D))/(2*A)
+# print(X1)
+# print(X2)
+
+# 40-masala
+# A1 = int(input())
+# B1 = int(input())
+# A2 = int(input())
+# B2 = int(input())
+# C1 = int(input())
+# C2 = int(input())
+# D = (A1*B2-A2*B1)
+# x = (C1*B2-C2*B1)/D
+# y = (A1*C2-A2*C1)/D
+# print(x)
+# print(y)
+
